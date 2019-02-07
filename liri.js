5ab9ffa1b535bf9d29f90fc8298685f050f0d1aa
setheugene <seth.eugene@icloud.com>
2019-02-07 11:28:43 -0600

    working

--- liri.js
+++ liri.js
@@ -1,31 +1,106 @@
-require("dotenv").config();
+var env = require("dotenv").config()
 
-var keys = require("./keys.js");
+var keys = require("./keys.js")
+
+var Spotify = require('node-spotify-api');
 
 var spotify = new Spotify(keys.spotify);
 
-
 var axios = require("axios");
 
-var inquirer = require("inquirer");
+var moment = require('moment');
 
-inquirer.prompt([
-    {
-        type: "input",
-        message: "name a movie",
-        name: "input",
-    }
-])
-.then(function(response) {
-var movieName = response.input;
+moment().format();
 
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-console.log(queryUrl);
 
-axios.get(queryUrl).then(
-  function(response) {
-    console.log("Release Year: " + response.data.Year);
+var band = function(artist) {
+  var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+  if (artist === undefined) {
+    artist = "radiohead";
   }
-)}
-);
+
+  axios.get(queryUrl).then(
+    
+    function(response) {
+      // console.log(response);
+      for (i = 1; i < 6; i++) {
+          console.log("Concert " + [i])
+          console.log("Venue Name : " + response.data[i].venue.name)
+          console.log("Venue Location : " + response.data[i].venue.city + ", " + response.data[i].venue.country);
+          var date = response.data[i].datetime;
+          console.log("Date : " + moment(date).format("dddd, MMMM Do YYYY"));
+          console.log("\n ---------------- \n");
+      }
+    }
+  )
+}
+
+var input = process.argv[2];
+console.log (process.argv[2]);
+
+if (input === "concert-this") {
+  artist = process.argv[3];
+  band(artist);
+}
+
+
+// var omdb = function(movieSearch) {
+//   var queryUrl = "http://www.omdbapi.com/?t=" + movieSearch + "&y=&plot=short&apikey=trilogy";
+//   if (movieSearch === undefined) {
+//     movieSearch = "mr. nobody";
+//   }
+ 
+
+// axios.get(queryUrl).then(
+//   function(response) {
+//     // console.log(response);
+//     console.log(movieSearch);
+//     console.log("\n______________________________\n");
+//     console.log("Title : " + response.data.Title);
+//     console.log("Year : " + response.data.Year);
+//     console.log("IMDB Rating : " + response.data.Rated);
+//     console.log("RottenTomatoes Rating : " + response.data.Ratings[1].Value);
+//     console.log("Country : " + response.data.Country);
+//     console.log("Language : " + response.data.Language);
+//     console.log("Plot : " + response.data.Plot);
+//     console.log("Actors : " + response.data.Actors);
+//     console.log("\n -------------------- \n");
+//   }
+// );}
+
+// var songSearch = function(searchSong) {
+//   if (searchSong === undefined) {
+//     searchSong = "don't stop me now";
+//   }
+//   spotify.search (
+//     {
+//       type: "track",
+//       query: searchSong
+//     },
+//   ).then(function(response){
+//   console.log(response[0])
+//   console.log(response.tracks.items.length + " results found, displaying top 10");
+//   for (i = 0; i < 10; i++) {
+//     console.log("Result : " + i);
+//     console.log("Artist : " + response.tracks.items[i].album.artists[0].name);
+//     console.log("Song Name : " + response.tracks.items[i].album.name)
+//     console.log("Album Name : " + response.tracks.items[i].name);
+//     console.log("Preview : " + response.tracks.items[i].preview_url);
+//     console.log("\n-----------------------\n");
+// }
+// }).catch(function(err) {
+// console.log("Error : " + err);
+// })
+//   }
+
+
+  // var input = process.argv[2];
+  // console.log (process.argv[2]);
+
+  // if (input === "concert-this") {
+  //   artist = process.argv[3];
+  //   band();
+  // }
+
+  
